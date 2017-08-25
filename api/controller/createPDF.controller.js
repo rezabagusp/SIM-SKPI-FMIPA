7d62d6392f5e6d3f042db9fab3a1f6bddb51df3c
rezabagusp <rezabaguspermana.rbp@gmail.com>
2017-08-25 08:19:54 +0700

    optimasi dan perbaikan genereate pdf

--- api/controller/createPDF.controller.js
+++ api/controller/createPDF.controller.js
@@ -8,6 +8,9 @@ var sub_kategori = sequelize.import(__dirname + '/../models/sub_kategori.model')
 var bobot = sequelize.import(__dirname + '/../models/skor.model')
 var mahasiswa = sequelize.import(__dirname + '/../models/mahasiswa.model')
 var ekstrakurikuler = sequelize.import(__dirname + '/../models/ekstrakurikuler.model')
+var departemen = sequelize.import(__dirname + '/../models/departemen.model')
+
+mahasiswa.belongsTo(departemen, {foreignKey:'fk_departemen_id'})
 
 class GeneratePDF {
 	constructor() {
@@ -17,6 +20,7 @@ class GeneratePDF {
 		this.tanggalttd = ''
 		this.namadekan = ''
 		this.nipdekan = ''
+		this.idmahasiswa = ''
 		this.nama = ''
 		this.NIM = ''
 		this.prodi = ''
@@ -27,38 +31,13 @@ class GeneratePDF {
 		this.bobots = ''
 		this.ekstrakurikulers = ''
 		this.total = 0
-		this.jumlah = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
-		this.bukti = [0, 0, 0, 0, 0, 0]
+		this.bukti = 0
 		this.kesimpulan = ''
 		this.banyaksubperkategori = []
-		this.indexbukti = 0
-	}
-	/*Setup PDF pepper*/
-	Redefine() {
-		this.resource = ''
-		this.destination = ''
-		this.options = ''
-		this.tanggalttd = ''
-		this.namadekan = ''
-		this.nipdekan = ''
-		this.nama = ''
-		this.NIM = ''
-		this.prodi = ''
-		this.tanggallulus = ''
-		this.tingkats = ''
-		this.kategoris = ''
-		this.sub_kategoris = ''
-		this.bobots = ''
-		this.ekstrakurikulers = ''
-		this.total = 0
-		this.jumlah = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
-		this.bukti = [0, 0, 0, 0, 0, 0]
-		this.kesimpulan = ''
-		this.banyaksubperkategori = []
-		this.indexbukti = 0
+		this.indexekstrakurikuler = 0
 	}
 	InitGeneratePDF(data, res) {
-		this.Redefine()
+		// this.Redefine()
 		this.resource = fs.readFileSync(__dirname + '/dynamictamplate.html', 'utf8');
 		this.options = { 
 			"format": 'A4', 
@@ -89,18 +68,24 @@ class GeneratePDF {
 	}
 	/*Set identitas of mahasiswa*/
 	SetIdentitasMahasiswa(data, res/*need params*/) {
+		var nim = data.body.nim,
+			tanggal_lulus = moment(data.body.tanggal_lulus).locale('id').format('DD MMMM YYYY');
 		mahasiswa
 			.findOne({
 				where: {
 					/*sementara*/
-					id: 1 /*data.params.id*/
-				}
+					nim_mahasiswa: nim
+				},
+				include:[{
+					model: departemen
+				}]
 			})
 			.then((mahasiswas) => {
+				this.idmahasiswa = mahasiswas.id
 				this.nama = mahasiswas.nama_mahasiswa
 				this.NIM = mahasiswas.nim_mahasiswa
-				this.prodi = 'ilmu komputer'
-				this.tanggallulus = '2 Maret 2017'
+				this.prodi = mahasiswas.departemen.nama_departemen
+				this.tanggallulus = tanggal_lulus
 				this.resource = this.resource.replace('{{Nama}}', this.nama)
 				this.resource = this.resource.replace('{{Nama}}', this.nama)
 				this.resource = this.resource.replace('{{Nama}}', this.nama)
@@ -110,6 +95,7 @@ class GeneratePDF {
 				this.resource = this.resource.replace('{{Prodi}}', this.prodi)
 				this.resource = this.resource.replace('{{Prodi}}', this.prodi)
 				this.resource = this.resource.replace('{{Prodi}}', this.prodi)
+				this.resource = this.resource.replace('{{Prodi}}', this.prodi)				
 				this.resource = this.resource.replace('{{Tanggal}}', this.tanggallulus)
 				this.resource = this.resource.replace('{{Tanggal}}', this.tanggallulus)
 				this.GetDataSKPI(data, res)
@@ -131,13 +117,23 @@ class GeneratePDF {
 				/*Get Sub_kategori data (the role of kategori data)*/
 			})
 		sub_kategori
-			.findAll()
+			.findAll({
+				order: [
+					['fk_kategori_id'],
+					['id']
+				]
+			})
 			.then((sub_kategoris) => {
 				this.sub_kategoris = JSON.parse(JSON.stringify(sub_kategoris))
 				/*Get skor of the role witch specific kategori and sub_kategori*/
 			})
 		bobot
-			.findAll()
+			.findAll({
+				order: [
+					['fk_sub_kategori_id'],
+					['fk_tingkat_id']
+				]
+			})
 			.then((bobots) => {
 				this.bobots = JSON.parse(JSON.stringify(bobots))
 				/*Get mahasiswa ekstrakulikuler of specific mahasiswa*/
@@ -146,11 +142,12 @@ class GeneratePDF {
 			.findAll({
 				where: {
 					/*sementara*/
-					fk_mahasiswa_id: 1 /*data.params.id*/,
+					fk_mahasiswa_id: this.idmahasiswa,
 					status_verifikasi_ekstrakurikuler: 1
 				},
 				order: [
-					['fk_skor_id']
+					['fk_skor_id'],
+					['id']
 				]
 			})
 			.then((ekstrakurikulers) => {
@@ -161,6 +158,7 @@ class GeneratePDF {
 	}
 	/*Count sub-kategori/primary-katagori (ex: Kegiatan have 6 sub_kategori)*/
 	SetSubPerKategori(data, res) {
+		this.banyaksubperkategori = []
 		for(let i=0; i<this.kategoris.length; i++) {
 			let count = 0
 			for(let j=0; j<this.sub_kategoris.length; j++) {
@@ -176,14 +174,17 @@ class GeneratePDF {
 	SetPDFSKPI(data, res) {
 		/*Variable for number of kategori*/
 		let indexkategori = 0
-		let indextotal = 0
+		let indexawalsubkategori = 0
+		this.indexekstrakurikuler = 0
+		this.total = 0
 		for(let i=0; i<this.kategoris.length; i++) {
 			/*loop tingkat*/
 			for(let j=0; j<this.tingkats.length; j++) {
 				/*Write to tamplate HTML with let katgori*/
+				this.bukti = 0
 				let kategoris = `
 					<tr>
-						<td align="center" rowspan="`+ (this.banyaksubperkategori[i] + 2) +`">`+ (indexkategori + 1) +`</td>
+						<td align="center" rowspan="{{rowspankategori}}">`+ (indexkategori + 1) +`</td>
 					</tr>
 					<tr>
 						<th align="left" colspan="4">Mengikuti `+ this.kategoris[i].nama_kategori +` di Tingkat `+ this.tingkats[j].nama_tingkat +`</th>
@@ -192,59 +193,99 @@ class GeneratePDF {
 				`
 				/*function for replace {{kategoris}} with let kategoris*/
 				this.resource = this.resource.replace('{{kategoris}}', kategoris)
+				let kategorisbukti = `
+					<tr>
+						<td align="center" rowspan="{{rowspanbukti}}">`+ (indexkategori+1) +`</td>
+					</tr>
+					<tr>
+						<th align="left" colspan="5">Mengikuti `+ this.kategoris[i].nama_kategori +` di Tingkat `+ this.tingkats[j].nama_tingkat +`</th>
+					</tr>
+					{{daftar}}
+				`
+				this.resource = this.resource.replace('{{bukti}}', kategorisbukti)
 				let indexsubkategori = 1
 				/*Write tamplate sub_kategori for each kategori*/
-				for(let k=0; k<this.sub_kategoris.length; k++) {
-					if(this.sub_kategoris[k].fk_kategori_id == this.kategoris[i].id) {
-						var sub_kategoris = `
-							<tr>
-								<td class="isikegiatan">
-									`+ (indexsubkategori) +`.	`+this.sub_kategoris[k].nama_sub_kategori+`
-								</td>
-								<td class="angka">
-									{{bobot}}
-								</td>
-								<td class="angka">
-									{{jumlah}}
-								</td>
-								<td class="angka">
-									{{skor}}
-								</td>
-							</tr>
-							{{sub_kategoris}}						
-							`
-						this.resource = this.resource.replace('{{sub_kategoris}}', sub_kategoris)
-						this.LoopNilai(this.tingkats[j].id, this.sub_kategoris[k].id, indextotal, indexkategori)
-						indextotal++
-						indexsubkategori++
+				for(let k=0; k<this.banyaksubperkategori[i]; k++) {
+					if(this.sub_kategoris[k+indexawalsubkategori].fk_kategori_id == this.kategoris[i].id) {
+						if(this.bobots[this.tingkats.length * (this.sub_kategoris[k+indexawalsubkategori].id-1) + j] != undefined ) {
+							let skor = this.bobots[this.tingkats.length * (this.sub_kategoris[k+indexawalsubkategori].id-1) + j]
+							var sub_kategoris = `
+								<tr>
+									<td class="isikegiatan">
+										`+ (indexsubkategori) +`.	`+this.sub_kategoris[k+indexawalsubkategori].nama_sub_kategori+`
+									</td>
+									<td class="angka">
+										`+ skor.skor +`
+									</td>
+									<td class="angka">
+										{{jumlah}}
+									</td>
+									<td class="angka">
+										{{skor}}
+									</td>
+								</tr>
+								{{sub_kategoris}}						
+								`
+							this.resource = this.resource.replace('{{sub_kategoris}}', sub_kategoris)
+							this.LoopData(skor, indexkategori)
+							indexsubkategori++
+						}
 					}
 				}
+				this.resource = this.resource.replace('{{rowspankategori}}', indexsubkategori + 1)
+				this.resource = this.resource.replace('{{rowspanbukti}}', this.bukti + 2)
 				this.resource = this.resource.replace('{{sub_kategoris}}', '{{kategoris}}')
-				this.LoopBukti(this.tingkats[j].id, indexkategori, i)
+				this.resource = this.resource.replace('{{daftar}}', '{{bukti}}')
 				indexkategori++
 			}
+			indexawalsubkategori += this.banyaksubperkategori[i]
 		}
 		this.resource = this.resource.replace('{{kategoris}}', '')
 		this.SetSummary(data, res)
 	}
 	/*Put the skor of SKPI in page 1*/
-	LoopNilai(id_tingkat, id_sub_kategori, index, indextingkat) {
-		var count = 0
-		for(let k=0; k<this.ekstrakurikulers.length; k++) {
-			if(this.ekstrakurikulers[k].fk_skor_id == (index+1)) {
+	LoopData(skor, indexkategori) {
+		let count = 0
+		if(this.ekstrakurikulers.length != this.indexekstrakurikuler) {
+			while(this.ekstrakurikulers[this.indexekstrakurikuler].fk_skor_id == skor.id) {
+				/*Set waktu in SKPI*/
+				if(this.ekstrakurikulers[this.indexekstrakurikuler].tanggal_mulai == this.ekstrakurikulers[this.indexekstrakurikuler].tanggal_selesai) {
+					var tanggal = moment(this.ekstrakurikulers[this.indexekstrakurikuler].tanggal_mulai).locale('id').format('D MMMM YYYY')
+				} else if(moment(this.ekstrakurikulers[this.indexekstrakurikuler].tanggal_mulai).format('MMM') != moment(this.ekstrakurikulers[this.indexekstrakurikuler].tanggal_selesai).format('MMM')) {
+					if(moment(this.ekstrakurikulers[this.indexekstrakurikuler].tanggal_mulai).format('YY') != moment(this.ekstrakurikulers[this.indexekstrakurikuler].tanggal_selesai).format('YY')) {
+						var tanggal = moment(this.ekstrakurikulers[this.indexekstrakurikuler].tanggal_mulai).locale('id').format('D MMMM YYYY') + ' - ' +  moment(this.ekstrakurikulers[this.indexekstrakurikuler].tanggal_selesai).locale('id').format('D MMMM YYYY')
+					} else {
+						var tanggal = moment(this.ekstrakurikulers[this.indexekstrakurikuler].tanggal_mulai).locale('id').format('D MMMM') + ' - ' +  moment(this.ekstrakurikulers[this.indexekstrakurikuler].tanggal_selesai).locale('id').format('D MMMM YYYY')		
+					}
+				} else {
+					var tanggal = moment(this.ekstrakurikulers[this.indexekstrakurikuler].tanggal_mulai).locale('id').format('D') + ' - ' +  moment(this.ekstrakurikulers[this.indexekstrakurikuler].tanggal_selesai).locale('id').format('D MMMM YYYY')
+				}
+				var bukti = `
+					<tr>
+						<td align="center">`+ (count+1) +`</td>
+						<td align="center">`+ this.ekstrakurikulers[this.indexekstrakurikuler].nama_ekstrakurikuler +`</td>
+						<td align="center">`+ tanggal +`</td>
+						<td align="center">`+ skor.skor +`</td>
+						<td align="center"> ada </td>
+					</tr>
+					{{daftar}}
+					`
+				this.resource = this.resource.replace('{{daftar}}', bukti)
 				count++
+				this.indexekstrakurikuler++
+				if(this.ekstrakurikulers.length == this.indexekstrakurikuler) {
+					break
+				}
 			}
 		}
-		this.resource = this.resource.replace('{{bobot}}', this.bobots[index].skor)
 		if(count == 0){
 			this.resource = this.resource.replace('{{jumlah}}', '')
 		} else {
 			this.resource = this.resource.replace('{{jumlah}}', count)
 		}
-		this.jumlah[index] = count
-		this.resource = this.resource.replace('{{skor}}', this.jumlah[index]*this.bobots[index].skor)
-		this.total += count*this.bobots[index].skor
-		this.bukti[indextingkat] += count
+		this.resource = this.resource.replace('{{skor}}', count*skor.skor)
+		this.total += count*skor.skor
+		this.bukti += count
 	}
 	/*Set simpulan and total skor of SKPI*/
 	SetSummary(data, res) {
@@ -258,58 +299,12 @@ class GeneratePDF {
 			this.kesimpulan = 'Cukup'
 		} else if(this.total > 5) {
 			this.kesimpulan = 'Kurang'
+		} else {
+			this.kesimpulan = ''
 		}
 		this.resource = this.resource.replace('{{kesimpulan}}', this.kesimpulan)
 		this.resource = this.resource.replace('{{kesimpulan}}', this.kesimpulan)
 		this.ConvertHTLM2PDF(data, res)
-	}
-	/*Put lampiran in the next page*/
-	/*NOTE: imnot sure with this method you can rebuild for the fastest result*/
-	LoopBukti(id_tingkat, indexkategori, i) {
-		let kategoris = `
-			<tr>
-				<td align="center" rowspan="`+ (2+this.bukti[indexkategori]) +`">`+ (indexkategori+1) +`</td>
-			</tr>
-			<tr>
-				<th align="left" colspan="5">Mengikuti `+ this.kategoris[i].nama_kategori +` di Tingkat `+ this.tingkats[id_tingkat-1].nama_tingkat +`</th>
-			</tr>
-			{{daftar}}
-		`
-		this.resource = this.resource.replace('{{bukti}}', kategoris)
-		var no = 1
-		for(let k=this.indexbukti; k<this.indexbukti+this.bukti[indexkategori]; k++) {
-			if(this.ekstrakurikulers[k].bukti_ekstrakurikuler != null) {
-				var say = 'Ada'
-			} else {
-				var say = 'Tidak'
-			}
-			/*Set waktu in SKPI*/
-			if(this.ekstrakurikulers[k].tanggal_mulai == this.ekstrakurikulers[k].tanggal_selesai) {
-				var tanggal = moment(this.ekstrakurikulers[k].tanggal_mulai).locale('id').format('D MMMM YYYY')
-			} else if(moment(this.ekstrakurikulers[k].tanggal_mulai).format('MMM') != moment(this.ekstrakurikulers[k].tanggal_selesai).format('MMM')) {
-				if(moment(this.ekstrakurikulers[k].tanggal_mulai).format('YY') != moment(this.ekstrakurikulers[k].tanggal_selesai).format('YY')) {
-					var tanggal = moment(this.ekstrakurikulers[k].tanggal_mulai).locale('id').format('D MMMM YYYY') + ' - ' +  moment(this.ekstrakurikulers[k].tanggal_selesai).locale('id').format('D MMMM YYYY')
-				} else {
-					var tanggal = moment(this.ekstrakurikulers[k].tanggal_mulai).locale('id').format('D MMMM') + ' - ' +  moment(this.ekstrakurikulers[k].tanggal_selesai).locale('id').format('D MMMM YYYY')		
-				}
-			} else {
-				var tanggal = moment(this.ekstrakurikulers[k].tanggal_mulai).locale('id').format('D') + ' - ' +  moment(this.ekstrakurikulers[k].tanggal_selesai).locale('id').format('D MMMM YYYY')
-			}
-			var bukti = `
-			<tr>
-				<td align="center">`+ no +`</td>
-				<td align="center">`+ this.ekstrakurikulers[k].nama_ekstrakurikuler +`</td>
-				<td align="center">`+ tanggal +`</td>
-				<td align="center">`+ this.bobots[this.ekstrakurikulers[k].fk_skor_id-1].skor +`</td>
-				<td align="center">`+ say +`</td>
-			</tr>
-			{{daftar}}
-			`
-			this.resource = this.resource.replace('{{daftar}}', bukti)
-			no++
-		}
-		this.indexbukti += this.bukti[indexkategori]
-		this.resource = this.resource.replace('{{daftar}}', '{{bukti}}')
 	}
 	/*Convert HTML to PDF*/
 	ConvertHTLM2PDF(data, res) {
