c6389eab2618f44f110d3e845b0bb1a9f9d4f364
rezabagusp <rezabaguspermana.rbp@gmail.com>
2017-10-11 19:08:22 +0700

    penambahan redirect callback SSO oauth2

--- api/routes/login.js
+++ api/routes/login.js
@@ -7,7 +7,15 @@ router.post('/masuk', function(req, res, next){
 });
 
 router.get('/auth2', function(req, res, next){
-    auth.auth2(req.body, res);
+    auth.auth2(req, res);
+});
+
+router.get('/logoutauth2', function(req, res, next){
+    auth.logoutAuth2(req, res);
+});
+
+router.get('/openssopage', function(req, res, next){
+    auth.openSsoPage(req, res);
 });
 
 
